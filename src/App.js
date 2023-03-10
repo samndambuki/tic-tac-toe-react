eaa09c13ebacf38d2fba79a554dad163210d8843
samndambuki <sam.ndambuki08@gmail.com>
2023-03-10 14:48:52 +0300

    lifting-state-up incomplete

--- src/App.js
+++ src/App.js
@@ -1,17 +1,18 @@
 import { useState } from "react";
 
 
-function Square(){
+function Square({value,onSquareClick}){
 
-  const [value,setValue] = useState(null); //value is a state variable
-
-  function handleClick(){
-    setValue('X'); //event handler
-  }
+  // const [value,setValue] = useState(null); //value is a state variable
+  
+  // function handleClick(){
+  //   setValue('X'); //event handler
+  // }
   return(
   <button 
   className="square"
-  onClick={handleClick}
+  // onClick={handleClick}
+  onClick={onSquareClick}
   >
   {value}
   </button>
@@ -19,25 +20,28 @@ function Square(){
 }
 
 export default function Board(){
+
+  const [squares, setSquares] = useState(Array(9).fill(null));
+
   return(
   <>
 
   <div className="board-row">
-    <Square/>
-    <Square/>
-    <Square/>
+    <Square value={squares[0]}/>
+    <Square value={squares[1]}/>
+    <Square value={squares[2]}/>
   </div>
   
   <div className="board-row">
-    <Square/>
-    <Square/>
-    <Square/>
+    <Square value={squares[3]}/>
+    <Square value={squares[4]}/>
+    <Square value={squares[5]}/>
   </div> 
 
   <div className="board-row">
-    <Square/>
-    <Square/>
-    <Square/>
+    <Square value={squares[6]}/>
+    <Square value={squares[7]}/>
+    <Square value={squares[8]}/>
   </div>
   
   </> 
